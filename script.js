48889f6a2ab5fbfb4443617c7f37e978f2878116
Krishna Uprit <125099508+Krishnauprit18@users.noreply.github.com>
2024-02-18 18:31:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
     simulationForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        // Your existing form validation logic
         const selectedSimulationTypes = simulationTypeForm.querySelectorAll('input[name="simulation_type"]:checked');
         if (selectedSimulationTypes.length === 0) {
             alert('Please select at least one simulation type.');
@@ -28,26 +27,22 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        // Function to validate IP address format
         function isValidIpAddress(ip) {
             const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
             return ipRegex.test(ip);
         }
 
-        // Function to validate port format
         function isValidPort(port) {
             const portRegex = /^(?!0)([0-9]{1,5})$/;
             return portRegex.test(port);
         }
 
-        // Use AJAX to submit the form data
         fetch(simulationForm.action, {
             method: 'POST',
             body: new FormData(simulationForm),
         })
         .then(response => response.text())
         .then(data => {
-            // Update the result or error section based on the response
             document.getElementById('simulationResults').innerHTML = data;
         })
         .catch(error => {
