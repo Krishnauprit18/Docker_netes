38fd9523339056adad14775cc3c29bf11b655c0f
Krishna Uprit <125099508+Krishnauprit18@users.noreply.github.com>
2024-02-18 18:27:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from ddos_dectection import predict_ddos  # Import your ML model function
+from ddos_dectection import predict_ddos  
 
 app = Flask(__name__)
 
@@ -10,15 +10,13 @@ def index():
 @app.route('/simulate', methods=['POST'])
 def simulate():
     try:
-        # Retrieve data from the form
+        # Retrieving our data
         simulation_type = request.form.get('simulation_type')
         target_ip = request.form.get('target_ip')
         target_port = request.form.get('target_port')
 
-        # Perform DDoS prediction using your ML model
+        # Performing DDoS prediction using our ML model
         ddos_prediction = predict_ddos(target_ip, target_port)
-
-        # You can handle the prediction and return relevant information
 
         return render_template('result.html', prediction=ddos_prediction)
 
