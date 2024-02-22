bfcd46e95c4bca406560243b9bcab1f6914c3e40
Krishna Uprit <125099508+Krishnauprit18@users.noreply.github.com>
2024-02-22 12:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,11 @@ def index():
 @app.route('/simulate', methods=['POST'])
 def simulate():
     try:
-        # Retrieve data from the form
         simulation_type = request.form.get('simulation_type')
         target_ip = request.form.get('target_ip')
         target_port = request.form.get('target_port')
 
-        # Perform DDoS prediction using your ML model
         ddos_prediction = predict_ddos(target_ip, target_port)
-
-        # You can handle the prediction and return relevant information
-
         return render_template('result.html', prediction=ddos_prediction)
 
     except Exception as e:
